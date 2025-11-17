47d3f16d4e5f0638426e657affa86c390368d9e5
Touya (nvatuan) <lambda.nvat@gmail.com>
2025-11-17 21:47:06 +0900

    Fix GitHub Comment signature not properly replaced

--- src/pkg/template/constants.go
+++ src/pkg/template/constants.go
@@ -3,7 +3,8 @@ package template
 // DefaultCommentTemplate is the embedded default template for PR comments
 // This template supports MultiEnvCommentData structure
 const (
-	ToolCommentSignature    = `<!-- gitops-kustomzchk: {{.Service}} - auto-generated comment, please do not remove -->`
+	ToolCommentServiceToken = "$SERVICE$"
+	ToolCommentSignature    = `<!-- gitops-kustomzchk: $SERVICE$ - auto-generated comment, please do not remove -->`
 	FileNameCommentTemplate = "comment.md.tmpl"
 	FileNameDiffTemplate    = "diff.md.tmpl"
 	FileNamePolicyTemplate  = "policy.md.tmpl"
